a11683ce4e062baabae040c5fca81728bb1c3c11
nicolas.delafuente <ni.delafuente@gmail.com>
2023-11-22 16:39:20 -0300

    Refactorizacion

--- main.py
+++ main.py
@@ -5,15 +5,3 @@ f = open("./entrada.txt", "r")
 input_text = f.read()
 print(input_text)
 parser.parse(input_text)
-
-def procesar_archivo():
-    with open('./entrada.txt', 'r') as file:
-        contenido = file.read()
-        # Realizar operaciones con el contenido del archivo (procesamiento)
-
-        # Ejemplo: Imprimir el contenido en consola
-        print(contenido)
-
-
-if __name__ == "__main__":
-    procesar_archivo()
--- src/commands.py
+++ src/commands.py
@@ -11,29 +11,14 @@ def navigate_to_url(url):
     global current_url
     current_url = url
     driver.get(url)
-    sleep(15)
-
-def get_text(element_class, element_text):
-    try:
-        elements = driver.find_elements(By.XPATH, element_class)
-        
-        assert_ = False
-
-        for element in elements:
-            if (element.get_attribute('innerText') == element_text) :
-                assert_ = True        
-        
-        print (f"'{element_text}' encontrado: '{assert_}'")
-    
-    except Exception as e:
-        print(f"Error al obtener el texto del elemento de la CLASE '{element_class}': {e}")
+    sleep(3)
 
 def click_element(element_id):
     try:
         element = driver.find_element(By.XPATH, element_id)
         print(element)
         element.click()
-        sleep(10)
+        sleep(3)
     except Exception as e:
         print(f"Error al hacer clic en el elemento con ID '{element_id}': {e}")
 
@@ -41,6 +26,40 @@ def set_text(element_id, text):
     try:
         element = driver.find_element(By.XPATH, element_id)
         element.send_keys(text)
-        sleep(10)
+        sleep(3)
     except Exception as e:
         print(f"Error al establecer el texto en el elemento con ID '{element_id}': {e}")
+
+def find_elements(xpath):
+    try:
+        elements = driver.find_elements(By.XPATH, xpath)
+        return elements
+    except Exception as e:
+        print(f"Error al encontrar elementos con la expresión XPath '{xpath}': {e}")
+        return []
+
+def get_element_text(element):
+    try:
+        return element.get_attribute('innerText')
+    except Exception as e:
+        print(f"Error al obtener el texto del elemento: {e}")
+        return None
+
+def assert_element_text(element_text, expected_text):
+    return element_text == expected_text
+
+def get_text(element_, element_text):
+    try:
+        elements = find_elements(element_)
+        
+        assert_ = False
+
+        for element in elements:
+            text = get_element_text(element)
+            if text is not None and assert_element_text(text, element_text):
+                assert_ = True
+
+        print(f"'{element_text}' encontrado: '{assert_}'")
+
+    except Exception as e:
+        print(f"Error al obtener el texto del elemento'{element_}': {e}")
